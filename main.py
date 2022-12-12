aefb4c32ae16a155729dd73c6e2a322452618ad2
Thunder-dt <thunderm.dt@gmail.com>
2022-12-12 10:51:55 +0100

    Functions File
    Game Mode choosing

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 print("--------------------------Game--------------------------")
 
+print("Choose the game mode: \nGame Mode 01 => Player 01 vs Player 02")
+print("Game Mode 02 => Player 01 vs AI ")
+print("Game Mode 03 => Ai vs Player 01 ")
+
+gameMode = input("Game Mode : ")
+gameMode = int(gameMode)
+
+
